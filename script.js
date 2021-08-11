90078bdecddd004af0b71937d4c482b1f10a33c9
Cory <co.ry@icloud.com>
2021-08-10 21:15:11 -0400

    loop started

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-playerSelection = "rock";
+let playerInput = prompt("Rock, paper or scissors")
+let playerSelection = playerInput.toLowerCase
 let computerSelection
 const myArray = ["rock", "paper", "scissors"];
 
-function computerRandom() {
-    computerSelection = myArray[Math.floor(Math.random() * myArray.length)];
-    return computerSelection
-}
-function playRound(computerSelection, playerSelection) {
-    if (computerSelection == playerSelection) {
-        return("tie game ")
-            }
-    else if (
-            (computerSelection == "rock" && playerSelection == "scissors") ||
-            (computerSelection == "scissors" && playerSelection == "paper") ||
-            (computerSelection == "paper" && playerSelection == "rock")
-            ) {
-            return("You lose")
-            }
-    else {
-        return("winner")
-            }
-}
-console.log(computerRandom())
-console.log(playerSelection)
+for(i=0; i<5; i++) {
+    function computerRandom() {
+        computerSelection = myArray[Math.floor(Math.random() * myArray.length)];
+        return computerSelection
+    }
+    function playRound(computerSelection, playerSelection) {
+        if (computerSelection == playerSelection) {
+            return("tie game ")
+                }
+        else if (
+                (computerSelection == "rock" && playerSelection == "scissors") ||
+                (computerSelection == "scissors" && playerSelection == "paper") ||
+                (computerSelection == "paper" && playerSelection == "rock")
+                ) {
+                return("You lose")
+                }
+        else {
+            return("winner")
+                }
+    }
+    console.log(computerRandom())
+    console.log(playerSelection)
 
-console.log(playRound(computerSelection, playerSelection));
+    console.log(playRound(computerSelection, playerSelection));
+}
