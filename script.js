daa7e318b0e31bb35e9873081b6159a5b1e9f37d
Cory <co.ry@icloud.com>
2021-08-11 21:38:59 -0400

    5 game loop added

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-let playerInput = prompt("Rock, paper or scissors")
-let playerSelection = playerInput.toLowerCase
-let computerSelection
+let playerInput;
+let playerSelection = playerInput;
+let computerSelection;
+let playerScore = 0
+let computerScore = 0
 const myArray = ["rock", "paper", "scissors"];
 
-for(i=0; i<5; i++) {
-    function computerRandom() {
-        computerSelection = myArray[Math.floor(Math.random() * myArray.length)];
-        return computerSelection
-    }
-    function playRound(computerSelection, playerSelection) {
-        if (computerSelection == playerSelection) {
-            return("tie game ")
-                }
-        else if (
-                (computerSelection == "rock" && playerSelection == "scissors") ||
-                (computerSelection == "scissors" && playerSelection == "paper") ||
-                (computerSelection == "paper" && playerSelection == "rock")
-                ) {
-                return("You lose")
-                }
-        else {
-            return("winner")
-                }
-    }
-    console.log(computerRandom())
-    console.log(playerSelection)
+function computerRandom() {
+    computerSelection = myArray[Math.floor(Math.random() * myArray.length)];
+    return computerSelection
+}
 
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection == playerSelection) {
+        return("tie game ")
+            }
+    else if (
+            (computerSelection == "rock" && playerSelection == "scissors") ||
+            (computerSelection == "scissors" && playerSelection == "paper") ||
+            (computerSelection == "paper" && playerSelection == "rock")
+            ) {
+            computerScore++    
+            return("You lose")
+            }
+    else {
+        playerScore++
+        return("winner")
+            }
+}
+
+for(i=0; i<5; i++) {
+    let playerInput = prompt("Rock, paper or scissors")
+    console.log(computerRandom());
+    console.log(playerSelection);
     console.log(playRound(computerSelection, playerSelection));
+    
+}
+console.log("total score>>>> computer: " + computerScore + " player: " + playerScore)
+
+if (playerScore > computerScore){
+    console.log("You are the grand champion")
+}
+else if(playerScore = computerScore){
+    console.log("Too close to call. Play again")
+}
+else {
+    console.log("You are a loser this time :(")
 }
